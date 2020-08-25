9f1157690324924342e3c981317b0adcf1ec579f
Andrés Sánchez <andresanchez1604@gmail.com>
2020-08-25 14:55:00 -0400

    Diseño y correcciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-const pokeInfoDisplay = document.querySelector('.pokeMain')
-const searchPoke = document.getElementById('srchPoke')
-const pokeTypes = document.querySelector('pokeType')
+const pokeInfoDisplay = document.querySelector('.pokeMainInfo')
+const searchPokeInput = document.getElementById('srchPokeInput')
+const searchButton = document.getElementById('srchButton')
 
-document.getElementById('srchButton').addEventListener("click", () => {
-    let query = searchPoke.value
+
+const search = () => {
+    let query = searchPokeInput.value
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
         .then(response => response.json())
@@ -14,25 +15,92 @@ document.getElementById('srchButton').addEventListener("click", () => {
         console.log(pokeData)
 
         pokeInfoDisplay.innerHTML = `
-        <h3 class="pokeName">${pokeData.name}</h3>
-        <h3 class="pokeNumber">#${pokeData.id}</h3>
+        <span class="pokeName">${pokeData.name}</span>
+        <span class="pokeNumber">#${pokeData.id}</span>
+        <div class="pokeForms">
+        <div class="normalForm">
+        <span>Normal Form</span><br>
         <img src="${pokeData.sprites.front_default}">
-        <span class="normalForm">Normal Form</span>
-        <img src="${pokeData.sprites.front_shiny}"
-        <span class="shinyForm">Shiny Form</span>
-        <ul class="pokeType"></ul>
+        </div>
+        <div class="shinyForm">
+        <span>Shiny Form</span><br>
+        <img src="${pokeData.sprites.front_shiny}">     
+        </div>
+        </div>
+        <div class="pokeType"></div>
+        <div class="pokeAbility"></div>
         `
-        
-        for (tipo of pokeData.types) {
-            
-            const typeList = document.createElement('li')
-            typeList.textContent = tipo.type.name
-            pokeTypes.insertAdjacentElement("beforebegin", typeList)
+        const pokeTypes = document.querySelector('.pokeType')
+
+        const dataTypes = pokeData['types'];
+        const dataFirstType = dataTypes[0];
+        const dataSecondType = dataTypes[1];
+
+        if (dataSecondType) {
+            pokeTypes.innerHTML = `
+            <span>Types:</span>
+            <span class="types">${dataFirstType.type.name}</span>
+            <span class="types">${dataSecondType.type.name}</span>
+            `
+        } else {
+            pokeTypes.innerHTML = `
+            <span>Type:</span>
+            <span class="types">${dataFirstType.type.name}</span>
+            `
         }
 
+        const pokeAbilities = document.querySelector('.pokeAbility')
+
+        const dataAbilities = pokeData['abilities']
+        const dataAbiliyOne = dataAbilities[0]
+        const dataAbiliyTwo = dataAbilities[1]
+        const dataAbiliyThree = dataAbilities[2]
+
+        
+        if (dataAbiliyThree) {
+            pokeAbilities.innerHTML = `
+            <div class="abilities">
+            <span>Abilities:</span>
+            <span class="ability">${dataAbiliyOne.ability.name.replace(/-/g, ' ')}</span>
+            <span class="ability">${dataAbiliyTwo.ability.name.replace(/-/g, ' ')}</span>
+            </div>
+            <div class="hiddenAbility">
+            <span>Hidden Ability:</span>
+            <span class="ability">${dataAbiliyThree.ability.name.replace(/-/g, ' ')}</span>
+            </div>
+            `
+        } else if (dataAbiliyTwo) {
+            pokeAbilities.innerHTML =`
+            <div class="abilities">
+            <span>Ability:</span>
+            <span class="ability">${dataAbiliyOne.ability.name.replace(/-/g, ' ')}</span>
+            </div>
+            <div class="hiddenAbility">
+            <span>Hidden Ability:</span>
+            <span class="ability">${dataAbiliyTwo.ability.name.replace(/-/g, ' ')}</span>
+            </div>
+            `
+        } else {
+            pokeAbilities.innerHTML =`
+            <div class="abilities">
+            <span>Ability:</span>
+            <span class="ability">${dataAbiliyOne.ability.name.replace(/-/g, ' ')}</span>
+            </div>
+            `
+        }
+        
     }
 
-})
+}
+
+const searchWithKey = event => {
+    if (event.key === "Enter") {
+        search()
+    }
+}
+
+searchButton.addEventListener('click', search)
+searchPokeInput.addEventListener('keydown', searchWithKey)
 
 
 
